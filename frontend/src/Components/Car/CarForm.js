ab1f8731df50a1a71d85724a6efc574d08073af7
ayu-k <45375782+ayu-k@users.noreply.github.com>
2020-08-06 18:42:03 +0900

    Create form component

--- frontend/src/Components/Car/CarForm.js
+++ frontend/src/Components/Car/CarForm.js
@@ -7,9 +7,23 @@ class CarForm extends React.Component {
     }
     render() { 
         return (
-            <div>
-                Car Form
-            </div>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="inputName">Name</label>
+                    <input type="text" className="form-control" id="name" ref="name" />
+                </div>
+                <div class="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputBrand">Brand</label>
+                        <input type="text" className="form-control" id="brand" ref="brand" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputYear">Year</label>
+                        <input type="text" className="form-control" id="year" ref="year" />
+                    </div>
+                 </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
          );
     }
 }
