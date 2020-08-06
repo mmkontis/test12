01cb086ce149f824d6d0592f79014036f53d08db
ayu-k <45375782+ayu-k@users.noreply.github.com>
2020-08-06 17:48:16 +0900

    Define components

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CarList from './Car/CarList';
+import Car from './Car/Car';
+import CarForm from './Car/CarForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">Menu</div>
+        </div>
+        <div className="row">
+          <div className="col-3"><CarList /></div>
+          <div className="col-9"><Car /></div>
+        </div>
+        <div className="row">
+          <div className="col-12"><CarForm /></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
